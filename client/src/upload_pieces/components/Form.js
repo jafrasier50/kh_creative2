36e9afb75a8aff842584a81fd20bea5ed7914028
Joshua Frasier <jafrasier50@hotmail.com>
2018-11-21 15:07:59 -0500

    posting new item does show on front-end

--- client/src/upload_pieces/components/Form.js
+++ client/src/upload_pieces/components/Form.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 import axios from "axios"
+import styled from 'styled-components'
+
+const StyledPiece = styled.div`
+
+    background-color: lightgray;
+    padding: 15px;
+    max-width: 200px;
+    margin: 10px;
+    img{
+        max-width: 150px;
+    }
+
+
+
+
+
+`
 
 class UploadPieceForm extends Component {  
     constructor() {
         super()
+        this.getPieces()
     }
     state = {
         title: "",
@@ -11,14 +29,53 @@ class UploadPieceForm extends Component {
         category: "",
         img_url: "",
         availability: "",
-        price: ""
+        price: "",
+        renderedPieces: []
     }
 
     handleChange = (e) => {
         e.preventDefault()
-        var changed = {}
+        let changed = {}
         changed[e.target.name] = e.target.value
         this.setState(changed)
+    }
+
+    getPieces = async () => {
+        
+        let result = await axios.get("api/pieces")
+        let pieces = result.data.pieces
+
+
+        if(pieces && pieces.length){
+            let renderedPiecesArray = []
+            pieces.forEach((piece) => {
+                renderedPiecesArray.push(this.renderPiece(piece))
+            
+            } )
+
+            this.setState({renderedPieces: renderedPiecesArray})
+        }
+    
+    }
+    
+    renderPiece = (info) => {
+        const {
+            title,
+            description,
+            category,
+            img_url,
+            availability,
+            price
+        } = info
+
+        return (<StyledPiece>
+        <h5>{title}</h5>
+        <p>{description}</p>
+        <img src={img_url}/>
+        <strong>Availability: {availability}</strong>
+        <strong>Price: {price}</strong>
+
+    </StyledPiece>)
     }
 
     uploadFile = async e => {
@@ -40,11 +97,34 @@ class UploadPieceForm extends Component {
 
     formSubmit = async e => {
         e.preventDefault()
-        console.log("string",this.state)
+      
+        const {
+
+            title,
+            description,
+            category,
+            img_url,
+            availability,
+            price
+
+        } = this.state
         
-        var result = await axios.post("api/piece",this.state)
-        console.log(result)
+        let result = await axios.post("api/piece",{
+            title,
+            description,
+            category,
+            img_url,
+            availability,
+            price
+        } )
+
+        var newRenderedPieces = [...this.state.renderedPieces ]
+
+        newRenderedPieces.push(this.renderPiece(result.data.created))
+       
+        this.setState({renderedPieces: newRenderedPieces })
     }
+
     render () {
         return (
             <div>
@@ -57,6 +137,7 @@ class UploadPieceForm extends Component {
                    <input onChange={this.uploadFile} type="file" id="file" placeholder="Upload an Image"/>
                    <input type="submit"/>
                 </form> 
+                {this.state.renderedPieces}
             </div>
         )
     }
