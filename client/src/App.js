1b0a29ebf749ac5ebbb863ed42baf61b5b198660
james sewell <jamesjosephsewell@gmail.com>
2018-11-27 19:36:52 -0600

    formatted code in the src folder, added home link

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './App.css'
 import Navbar from './navbar/components/Navbar'
 import PiecesView from './all_pieces/components/PiecesView'
 import PiecesDashboardView from './pieces_dashboard/components/View'
 import Login from './auth/Login'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router>
-        <div className="App">
-        <Navbar />
-        <Route exact path="/pieces" component={PiecesView} />
-        <Route exact path="/pieces_dashboard" component={PiecesDashboardView} />
-        <Route exact path="/login" component={Login} />
+        <div className='App'>
+          <Navbar />
+          <Route exact path='/home' component={PiecesView} />
+          <Route
+            exact
+            path='/pieces_dashboard'
+            component={PiecesDashboardView}
+          />
+          <Route exact path='/login' component={Login} />
         </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- client/src/pieces_dashboard/components/View.js
+++ client/src/pieces_dashboard/components/View.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
-import axios from "axios"
+import axios from 'axios'
 import PiecesDashboard from './PiecesDashboard'
 import Cookies from 'universal-cookie'
 import { checkAuth } from '../../auth/checkAuth'
 const cookies = new Cookies()
 
 class PiecesDashboardView extends Component {
-    constructor(props){
-        super(props)
-       
-    }
+  constructor (props) {
+    super(props)
+  }
 
-    render () {
-        const { loggedIn } = this.props
-        return (
-            <div>
-                {loggedIn? <PiecesDashboard loggedIn={this.props.loggedIn}/> : <p>fucking sign in Kelly</p>}
-            </div>
-        )
-    }
+  render () {
+    const { loggedIn } = this.props
+    return (
+      <div>
+        {loggedIn
+          ? <PiecesDashboard loggedIn={this.props.loggedIn} />
+          : <p>fucking sign in Kelly</p>}
+      </div>
+    )
+  }
 }
 
 export default checkAuth(PiecesDashboardView)
