443bb1ccb1703c330565590eb8cca0322803bd30
Joshua Frasier <jafrasier50@hotmail.com>
2019-04-13 19:16:30 -0500

    added build

---
+++ client/build/precache-manifest.fed17ba5b032d6e2f54c6f7945c05fef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ff3459a234c9d96141ffc502e29773",
+    "url": "/static/media/linkedin.a8ff3459.png"
+  },
+  {
+    "revision": "7f9e7cb7155bcfa05456",
+    "url": "/static/css/main.ca21df5a.chunk.css"
+  },
+  {
+    "revision": "bc7a6a3e77036c22fcb8",
+    "url": "/static/js/1.bc7a6a3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37dced6a4820ca406351767f4613581f",
+    "url": "/static/media/sideDrawerBackground.37dced6a.jpg"
+  },
+  {
+    "revision": "ba9571af0b3169adaab124dce933fb61",
+    "url": "/static/media/instagram.ba9571af.png"
+  },
+  {
+    "revision": "7f9e7cb7155bcfa05456",
+    "url": "/static/js/main.7f9e7cb7.chunk.js"
+  },
+  {
+    "revision": "fa3325e847cf6b8978b33a14c32a999e",
+    "url": "/static/media/LandingPageImage.fa3325e8.jpg"
+  },
+  {
+    "revision": "692243180c5b7e772eff4df7d9302d35",
+    "url": "/static/media/bioBackground.69224318.jpg"
+  },
+  {
+    "revision": "6540362c04a1a0c9521872b890a43b13",
+    "url": "/static/media/ContactBackground.6540362c.jpg"
+  },
+  {
+    "revision": "be081ca0beeafbfe593ef044f013b5dc",
+    "url": "/static/media/backgroundImage.be081ca0.jpg"
+  },
+  {
+    "revision": "6c4840ff3507041c5ce074a7df467646",
+    "url": "/index.html"
+  }
+];
